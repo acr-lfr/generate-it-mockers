8070a970bcd3f9c39d11616f78ac8faf76855cab
John Carmichael <john@johnine.org>
2022-03-24 11:35:39 +0000

    include the faker package by the new name and vbump

--- generators/fakerGenerator.js
+++ generators/fakerGenerator.js
@@ -1,20 +1,20 @@
-const faker = require('faker')
-const fakerMethods = require('./fakerMethods')
+const { faker } = require('@faker-js/faker');
+const fakerMethods = require('./fakerMethods');
 
 module.exports = (type) => {
   const method = fakerMethods.find((m) => {
-    const typeSplit = type.split('.')
+    const typeSplit = type.split('.');
     if (typeSplit.length === 1) {
       // not full method name
-      return type.toLowerCase() === m.split('.')[1].toLowerCase()
+      return type.toLowerCase() === m.split('.')[1].toLowerCase();
     } else {
       // full method name
-      return type.toLowerCase() === m.toLowerCase()
+      return type.toLowerCase() === m.toLowerCase();
     }
-  })
+  });
   if (method) {
-    const methodSplitter = method.split('.')
-    return faker[methodSplitter[0]][methodSplitter[1]]()
+    const methodSplitter = method.split('.');
+    return faker[methodSplitter[0]][methodSplitter[1]]();
   }
-  return false
-}
+  return false;
+};
